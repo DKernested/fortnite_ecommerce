5eba6fd684a2a137f6eaeed71065941317300079
Dillon <Dkernested@hotmail.com>
2020-11-22 21:41:46 -0600

    Kyle's awesome fixes - Repairing Kaminari

--- app/admin/cosmetics.rb
+++ app/admin/cosmetics.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register Cosmetic do
-  permit_params :name, :description, :type, :rarity, :image
+  permit_params :name, :description, :category_id, :rarity, :image
 
   form do |f|
     f.semantic_errors
     f.inputs do
       f.input :name
       f.input :description
-      f.input :item_type
+      f.input :category
       f.input :rarity
       f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image, size: "200x200") : ""
     end
