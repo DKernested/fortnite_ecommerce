4f3aa58e0903bcbeda626bed41dc4fc8332c4649
Dillon <Dkernested@hotmail.com>
2020-11-26 09:52:45 -0600

    added taxes to checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,9 +1,11 @@
 class CheckoutController < ApplicationController
   def create
-    cosmetic = Cosmetic.find(session[:shopping_cart])
+    # cosmetic = Cosmetic.find(session[:shopping_cart])
     cosmetic_single = Cosmetic.find(params[:id])
 
-    if cosmetic.nil?
+    province_tax = Province.find_by(id: current_customer.province_id)
+
+    if cosmetic_single.nil?
       redirect_to root_path
       return
     end
@@ -16,7 +18,21 @@ class CheckoutController < ApplicationController
         amount:      cosmetic_single.price,
         currency:    "cad",
         quantity:    1
-      }],
+      },
+                             {
+                               name:        "PST",
+                               description: "#{province_tax.name} Provincial Sales Tax",
+                               amount:      (cosmetic_single.price * province_tax.pst_rate).round.to_i,
+                               currency:    "cad",
+                               quantity:    1
+                             },
+                             {
+                               name:        "GST/HST",
+                               description: "Government Sales Tax or Harmonized Sales Tax",
+                               amount:      (cosmetic_single.price * province_tax.gst_rate).round.to_i,
+                               currency:    "cad",
+                               quantity:    1
+                             }],
       success_url:          checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
       cancel_url:           checkout_cancel_url
     )
